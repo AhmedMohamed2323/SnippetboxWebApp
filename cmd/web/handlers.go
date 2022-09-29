5b208917993d9b64e66888d019a65c03bd607423
Your Name <you@example.com>
2022-09-29 14:56:00 -0400

    Working with Session Data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,6 +10,7 @@ import (
 	"webapp.net/snippetbox/pkg/models" // New import
 )
 
+
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Because Pat matches the "/" path exactly, we can now remove the manual check
 	// of r.URL.Path != "/" from this handler.
@@ -26,8 +27,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
-	// Pat doesn't strip the colon from the named capture key, so we need to
-	// get the value of ":id" from the query string instead of "id".
 	id, err := strconv.Atoi(r.URL.Query().Get(":id"))
 	if err != nil || id < 1 {
 		app.notFound(w)
@@ -44,10 +43,19 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	// Use the PopString() method to retrieve the value for the "flash" key.
+	// PopString() also deletes the key and value from the session data, so it
+	// acts like a one-time fetch. If there is no matching key in the session
+	// data this will return the empty string.
+	flash := app.session.PopString(r, "flash")
+
+	// Pass the flash message to the template.
 	app.render(w, r, "show.page.tmpl", &templateData{
+		Flash:   flash,
 		Snippet: s,
 	})
 }
+
 
 func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "create.page.tmpl", &templateData{
@@ -56,6 +64,7 @@ func (app *application) createSnippetForm(w http.ResponseWriter, r *http.Request
 	})
 }
 
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -63,28 +72,28 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Create a new forms.Form struct containing the POSTed data from the
-	// form, then use the validation methods to check the content.
 	form := forms.New(r.PostForm)
 	form.Required("title", "content", "expires")
 	form.MaxLength("title", 100)
 	form.PermittedValues("expires", "365", "7", "1")
 
-	// If the form isn't valid, redisplay the template passing in the
-	// form.Form object as the data.
 	if !form.Valid() {
 		app.render(w, r, "create.page.tmpl", &templateData{Form: form})
 		return
 	}
 
-	// Because the form data (with type url.Values) has been anonymously embedded
-	// in the form.Form struct, we can use the Get() method to retrieve
-	// the validated value for a particular form field.
 	id, err := app.snippets.Insert(form.Get("title"), form.Get("content"), form.Get("expires"))
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+
+	// Use the Put() method to add a string value ("Your snippet was saved
+	// successfully!") and the corresponding key ("flash") to the session
+	// data. Note that if there's no existing session for the current user
+	// (or their session has expired) then a new, empty, session for them
+	// will automatically be created by the session middleware.
+	app.session.Put(r, "flash", "Snippet successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
 }
