91c1fdf7a1b7534814ecded94d0ad1cdd6519324
Your Name <you@example.com>
2022-10-05 22:56:36 -0400

    Testing HTML Forms

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"html" // New import
 	"io"
 	"log"
 	"net/http"
 	"net/http/cookiejar" // New import
 	"net/http/httptest"
+	"net/url" // New import
+	"regexp"
 	"testing"
 	"time" // New import
 
@@ -13,6 +16,23 @@ import (
 
 	"github.com/golangcollege/sessions" // New import
 )
+
+// Define a regular expression which captures the CSRF token value from the
+// HTML for our user signup page.
+var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value='(.+)'>`)
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	// Use the FindSubmatch method to extract the token from the HTML body.
+	// Note that this returns an array with the entire matched pattern in the
+	// first position, and the values of any captured data in the subsequent
+	// positions.
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+
+	return html.UnescapeString(string(matches[1]))
+}
 
 func newTestApplication(t *testing.T) *application {
 	// Create an instance of the template cache.
@@ -69,20 +89,22 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 	return &testServer{ts}
 }
 
-// Implement a get method on our custom testServer type. This makes a GET
-// request to a given url path on the test server, and returns the response
-// status code, headers and body.
-func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
-	rs, err := ts.Client().Get(ts.URL + urlPath)
+// Create a postForm method for sending POST requests to the test server.
+// The final parameter to this method is a url.Values object which can contain
+// any data that you want to send in the request body.
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// Read the response body.
 	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
 	if err != nil {
 		t.Fatal(err)
 	}
 
+	// Return the response status, headers and body.
 	return rs.StatusCode, rs.Header, body
 }
